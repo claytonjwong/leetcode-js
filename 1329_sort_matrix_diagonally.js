d433d0a953f012a1f72ce850eeaaefe0339035b9
Clayton Wong <claytonjwong@gmail.com>
2020-01-25 18:08:15 -0800

    add concise 'key' for buckets

--- 1329_sort_matrix_diagonally.js
+++ 1329_sort_matrix_diagonally.js
@@ -5,6 +5,7 @@
  * A: https://leetcode.com/problems/sort-the-matrix-diagonally/discuss/490185/Javascript-and-C%2B%2B-solutions
  */
 
+ /*
 let diagonalSort = (A, m = {}) => {
     let M = A.length,
         N = A[0].length;
@@ -26,5 +27,24 @@ let diagonalSort = (A, m = {}) => {
     for (let i = 0; i < M; ++i)
         for (let j = 0; j < N; ++j)
             A[i][j] = m[key(i, j)][0], m[key(i, j)].shift();
+    return A;
+};
+*/
+
+let diagonalSort = (A, m = {}) => {
+    let M = A.length,
+        N = A[0].length;
+    for (let i = 0; i < M; ++i) {
+        for (let j = 0; j < N; ++j) {
+            if (!m[i - j])
+                m[i - j] = [];
+            m[i - j].push(A[i][j]);
+        }
+    }
+    for (let i = 0; i < M; ++i) m[i].sort((a, b) => a - b);
+    for (let j = 1; j < N; ++j) m[-j].sort((a, b) => a - b);
+    for (let i = 0; i < M; ++i)
+        for (let j = 0; j < N; ++j)
+            A[i][j] = m[i - j][0], m[i - j].shift();
     return A;
 };
