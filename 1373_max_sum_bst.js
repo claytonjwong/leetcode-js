d951b034eb843f6c15e30e699546616ca322936d
Clayton Wong <claytonjwong@gmail.com>
2020-03-26 17:14:02 -0700

    1373. Maximum Sum BST in Binary Tree (same as accepted C++ solution)

--- 1373_max_sum_bst.js
+++ 1373_max_sum_bst.js
@@ -6,16 +6,38 @@
  */
 
 // Runtime error occurs, but I think this is an OJ issue!
-let maxSumBST = (root, ans = 0) => {
+
+// let maxSumBST = (root, ans = 0) => {
+//     let go = (root, isBST = false, sum = 0) => {
+//         let L = { min: Infinity, max: -Infinity, isBST: false, sum: 0 },
+//             R = { min: Infinity, max: -Infinity, isBST: false, sum: 0 };
+//         if (root.left)  [L.min, L.max, L.isBST, L.sum] = go(root.left);
+//         if (root.right) [R.min, R.max, R.isBST, R.sum] = go(root.right);
+//         if ((!root.left  || (L.isBST && L.max < root.val))
+//          && (!root.right || (R.isBST && R.min > root.val))) {
+//             sum = root.val + L.sum + R.sum;
+//             ans = Math.max(ans, sum);
+//             isBST = true;
+//         }
+//         let minimum = Math.min(L.min, R.min, root.val),
+//             maximum = Math.max(L.max, R.max, root.val);
+//         return [minimum, maximum, isBST, sum];
+//     };
+//     go(root);
+//     return ans;
+// };
+
+let maxSumBST = (root, max = 0) => {
     let go = (root, isBST = false, sum = 0) => {
+        if (!root)
+            return [Infinity, -Infinity, true, 0];
         let L = { min: Infinity, max: -Infinity, isBST: false, sum: 0 },
             R = { min: Infinity, max: -Infinity, isBST: false, sum: 0 };
-        if (root.left)  [L.min, L.max, L.isBST, L.sum] = go(root.left);
-        if (root.right) [R.min, R.max, R.isBST, R.sum] = go(root.right);
-        if ((!root.left  || (L.isBST && L.max < root.val))
-         && (!root.right || (R.isBST && R.min > root.val))) {
+        [L.min, L.max, L.isBST, L.sum] = go(root.left);
+        [R.min, R.max, R.isBST, R.sum] = go(root.right);
+        if (L.isBST && R.isBST && L.max < root.val && root.val < R.min) {
             sum = root.val + L.sum + R.sum;
-            ans = Math.max(ans, sum);
+            max = Math.max(max, sum);
             isBST = true;
         }
         let minimum = Math.min(L.min, R.min, root.val),
@@ -23,5 +45,5 @@ let maxSumBST = (root, ans = 0) => {
         return [minimum, maximum, isBST, sum];
     };
     go(root);
-    return ans;
+    return max;
 };
