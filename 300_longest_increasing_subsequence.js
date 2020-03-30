93afbe3bae107450a3acba7e8db4ec5a0413d582
Clayton Wong <claytonjwong@gmail.com>
2020-03-30 11:09:45 -0700

    300. Longest Increasing Subsequence (concise code + QA header)

--- 300_longest_increasing_subsequence.js
+++ 300_longest_increasing_subsequence.js
@@ -1,22 +1,25 @@
+/*
+ * 300. Longest Increasing Subsequence
+ *
+ * Q: https://leetcode.com/problems/longest-increasing-subsequence/
+ * A: https://leetcode.com/problems/longest-increasing-subsequence/discuss/385203/C%2B%2B-and-Javascript-solutions
+ */
+
 /**
  * @param {number[]} A
  * @return {number}
  */
-var lengthOfLIS = (A, ans=1) => {
-    if (A.length == 0) {
+
+let lengthOfLIS = (A, max = 1) => {
+    let N = A.length;
+    if (N == 0)
         return 0;
-    }
-    const N = A.length;
-    var dp = Array(N).fill(1);
-    for (var j=1; j < N; ++j) {
-        for (var i=0; i < j; ++i) {
-            if (A[i] < A[j]) {
-                dp[j] = Math.max(dp[j], dp[i] + 1);
-            }
-        }
-        ans = Math.max(ans, dp[j]);
-    }
-    return ans;
+    let dp = Array(N).fill(1);
+    for (let j = 1; j < N; ++j)
+        for (let i = 0; i < j; ++i)
+            if (A[i] < A[j])
+                max = Math.max(max, dp[j] = Math.max(dp[j], 1 + dp[i]));
+    return max;
 };
 
 var A = [1,2,3];
