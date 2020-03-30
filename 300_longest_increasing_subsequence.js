8f05c9c045ada0c5c681773ff1e2f8673e57f8d3
Clayton Wong <claytonjwong@gmail.com>
2020-03-30 16:47:58 -0700

    300. Longest Increasing Subsequence (add top-down)

--- 300_longest_increasing_subsequence.js
+++ 300_longest_increasing_subsequence.js
@@ -10,18 +10,37 @@
  * @return {number}
  */
 
-let lengthOfLIS = (A, max = 1) => {
+let lengthOfLIS = (A, m = new Map()) => {
     let N = A.length;
     if (N == 0)
         return 0;
-    let dp = Array(N).fill(1);
-    for (let j = 1; j < N; ++j)
+    let go = j => {
+        if (m.has(j))
+            return m.get(j);
+        m.set(j, 1);
+        if (j == 0)
+            return m.get(j);
         for (let i = 0; i < j; ++i)
             if (A[i] < A[j])
-                max = Math.max(max, dp[j] = Math.max(dp[j], 1 + dp[i]));
-    return max;
+                m.set(j, Math.max(m.get(j), 1 + go(i)));
+        return m.get(j);
+    };
+    [...Array(N).keys()].forEach((_, j) => go(j));
+    return Math.max(...m.values());
 };
 
-var A = [1,2,3];
+// let lengthOfLIS = (A, max = 1) => {
+//     let N = A.length;
+//     if (N == 0)
+//         return 0;
+//     let dp = Array(N).fill(1);
+//     for (let j = 1; j < N; ++j)
+//         for (let i = 0; i < j; ++i)
+//             if (A[i] < A[j])
+//                 max = Math.max(max, dp[j] = Math.max(dp[j], 1 + dp[i]));
+//     return max;
+// };
+
+var A = [1,3,6,7,9,4,10,5,6];
 const ans = lengthOfLIS(A);
 console.log(ans);
