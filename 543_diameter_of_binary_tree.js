fb800ccf72116931bdca9f42aae6db8fd828ed32
Clayton Wong <claytonjwong@gmail.com>
2020-04-11 09:14:34 -0700

    543. Diameter of Binary Tree

---
+++ 543_diameter_of_binary_tree.js
@@ -0,0 +1,16 @@
+/*
+ * 543. Diameter of Binary Tree
+ *
+ * Q: https://leetcode.com/problems/diameter-of-binary-tree/
+ * A: https://leetcode.com/problems/diameter-of-binary-tree/discuss/115019/Simple-C%2B%2B-Recursive-Solution
+ */
+
+let diameterOfBinaryTree = (root, max = 0) => {
+    let go = (root) => {
+        let L = root.left  ? 1 + go(root.left)  : 0,
+            R = root.right ? 1 + go(root.right) : 0;
+        max = Math.max(max, L + R);
+        return Math.max(L, R);
+    };
+    return max;
+};
