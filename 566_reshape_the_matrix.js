6f34b5b99a3e14875688f9e8eee2d5d75e6ea2f6
Clayton Wong <claytonjwong@gmail.com>
2020-04-25 10:55:04 -0700

    566. Reshape the Matrix

--- 566_reshape_the_matrix.js
+++ 566_reshape_the_matrix.js
@@ -5,6 +5,20 @@
  * A: https://leetcode.com/problems/reshape-the-matrix/discuss/597018/Javascript-and-C%2B%2B-solutions
  */
 
+let matrixReshape = (A, M, N, line = []) => {
+    let p = A.length,
+        q = p ? A[0].length : 0;
+    if (p * q != M * N)
+        return A;
+    A.forEach(row => row.forEach(x => line.push(x)));
+    let ans = [...Array(M)].map(row => Array(N));
+    let k = 0;
+    for (let i = 0; i < M; ++i)
+        for (let j = 0; j < N; ++j)
+            ans[i][j] = line[k++];
+    return ans;
+};
+
 let matrixReshape = (A, M, N, f = Math.floor) => {
     let i = A.length,
         j = i ? A[0].length : 0;
