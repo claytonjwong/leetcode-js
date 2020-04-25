39b064917a29b5121025ec3f1ec23eda3f9fda40
Clayton Wong <claytonjwong@gmail.com>
2020-04-25 07:56:55 -0700

    530. Minimum Absolute Difference in BST

--- 530_min_abs_diff_BST.js
+++ 530_min_abs_diff_BST.js
@@ -5,19 +5,17 @@
  * A: https://leetcode.com/problems/minimum-absolute-difference-in-bst/discuss/595803/Javascript-and-C%2B%2B-solutions
  */
 
-let getMinimumDifference = (root, set = new Set(), min = Infinity) => {
+let getMinimumDifference = (root, A = [], min = Infinity) => {
     let go = root => {
         if (!root)
             return;
-        set.add(root.val);
         go(root.left);
+        A.push(root.val);
         go(root.right);
     }
     go(root);
-    [...set].sort((a, b) => a - b).forEach((_, i, A) => {
-        if (!i)
-            return;
-        min = Math.min(min, Math.abs(A[i - 1] - A[i]));
+    A.forEach((_, i, A) => {
+        if (i) min = Math.min(min, Math.abs(A[i - 1] - A[i]));
     });
     return min;
 };
