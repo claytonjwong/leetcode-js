2cf2f5bfcfaedf4f8791b991bfd95c82a041fda3
Clayton Wong <claytonjwong@gmail.com>
2020-04-26 11:05:31 -0700

    contest 186 (4671 / 11684)

--- contest186.js
+++ contest186.js
@@ -13,6 +13,21 @@
  */
 
 // AC
+let maxScore = (s, max = 0) => {
+    let A = s.split('').map(Number);
+    let N = A.length;
+    let L = Array(N),
+        R = Array(N);
+    for (let i = 0; i < N; ++i)
+        L[i] = (A[i] == 0) + (i > 0 ? L[i - 1] : 0);     // prefix sums from 👉 (L)eft-to-right
+    for (let i = N - 1; i >= 0; --i)
+        R[i] = (A[i] == 1) + (i < N - 1 ? R[i + 1] : 0); // prefix sums from 👈 (R)ight-to-left
+    for (let i = 1; i < N; ++i)
+        max = Math.max(max, L[i - 1] + R[i]);
+    return max;
+};
+
+// AC
 let maxScore = (S, max = 0) => {
     let N = S.length;
     let zero = Number(S[0] == '0'),
