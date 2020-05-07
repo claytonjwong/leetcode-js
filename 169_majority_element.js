a966cbd4f9abb493a955c84a55769960a0df4776
Clayton Wong <claytonjwong@gmail.com>
2020-05-07 08:48:20 -0700

    169. Majority Element

---
+++ 169_majority_element.js
@@ -0,0 +1,11 @@
+/*
+ * 169. Majority Element
+ *
+ * Q: https://leetcode.com/problems/majority-element/
+ * A: https://leetcode.com/problems/majority-element/discuss/618622/Javascript-and-C%2B%2B-solutions
+ */
+
+ let majorityElement = (A, m = new Map()) => {
+    A.forEach(x => m.set(x, 1 + (m.get(x) || 0)));
+    return [...m.entries()].filter(a => a[1] >= A.length / 2).map(a => a[0]);
+};
