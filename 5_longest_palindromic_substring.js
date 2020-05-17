1f9db24cdf16479b7f4f36d1295cb757ada0533d
Clayton Wong <claytonjwong@gmail.com>
2020-05-17 15:40:21 -0700

    5. Longest Palindromic Substring (additional solutions)

--- 5_longest_palindromic_substring.js
+++ 5_longest_palindromic_substring.js
@@ -10,27 +10,23 @@
 /*
 let longestPalindrome = s => {
     let N = s.length;
-    if (!N)
-        return '';
-    let max = 1,
-        ans = s[0];
+    if (!N) return '';
+    let ans = s[0];
     let best = (i, j) => {
-        if (max < j - i + 1) {
-            max = j - i + 1;             // +1 for i..j inclusive
-            ans = s.substring(i, j + 1); // +1 for j non-inclusive
-        }
+        if (ans.length < j - i + 1)      // +1 for i..j inclusive
+            ans = s.substring(i, j + 1); // +1 for i..j inclusive 🎯
     };
     let go = (i = 0, j = N - 1) => {
-        if (i + 1 >= j) {
+        if (j - i < 2) {
             if (s[i] == s[j]) {
                 best(i, j);
                 return true;
             }
             return false;
         }
-        go(i + 1, j);
-        go(i, j - 1);
-        if (go(i + 1, j - 1)) {
+        go(i + 1, j);           // case 1: the sub-problem without the character at index i
+        go(i, j - 1);           // case 2: the sub-problem without the character at index j
+        if (go(i + 1, j - 1)) { // case 3: the sub-problem without the character at index i and j
             if (s[i] == s[j]) {
                 best(i, j);
                 return true;
@@ -88,6 +84,7 @@ let longestPalindrome = (s, m = new Map()) => {
 */
 
 // brute-force bottom up: AC
+/*
 let longestPalindrome = (s, max = 1, ans = '') => {
     let N = s.length;
     if (!N) return ans;
@@ -102,6 +99,26 @@ let longestPalindrome = (s, max = 1, ans = '') => {
         expand(i, i + 1); // case 2: even length palindrome
     return ans;
 };
+*/
+
+// bottom-up DP: AC
+let longestPalindrome = s => {
+    let N = s.length;
+    if (!N) return '';
+    let ans = s[0];
+    let dp = [...Array(N)].map(row => Array(N).fill(false));
+    for (let j = 1; j < N; ++j) {
+        for (let i = j; i >= 0; --i) {
+            dp[i][j] = s[i] == s[j] && (j - i < 2 || dp[i + 1][j - 1]);
+            if (dp[i][j] && ans.length < j - i + 1)
+                ans = s.substring(i, j + 1); // +1 for i..j inclusive 🎯
+        }
+    }
+    return ans;
+};
+
+console.log(longestPalindrome('a'));
+console.log(longestPalindrome('ac'));
 console.log(longestPalindrome('bb'));
 console.log(longestPalindrome('babad'));
 console.log(longestPalindrome('cbbd'));
