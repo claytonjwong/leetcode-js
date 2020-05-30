f55dca64d0451ee769e752b8f704811d2590f46e
Clayton Wong <claytonjwong@gmail.com>
2020-05-30 11:12:44 -0700

    1460. Make Two Arrays Equal by Reversing Sub-arrays

---
+++ 1460_make_arrays_equal_by_reversing_subarrays.js
@@ -0,0 +1,11 @@
+/*
+ * 1460. Make Two Arrays Equal by Reversing Sub-arrays
+ *
+ * Q: https://leetcode.com/problems/make-two-arrays-equal-by-reversing-sub-arrays/
+ * A: https://leetcode.com/problems/make-two-arrays-equal-by-reversing-sub-arrays/discuss/660539/Javascript-and-C%2B%2B-solutions
+ */
+let canBeEqual = (T, A, m = new Map()) => {
+    T.forEach(x => m.set(x,  1 + (m.get(x) || 0)));
+    A.forEach(x => m.set(x, -1 + (m.get(x) || 0)));
+    return [...m.values()].every(x => !x);
+};
