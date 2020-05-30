99e67f9d5b076c6bc305a6a1e58e553f06e03abf
Clayton Wong <claytonjwong@gmail.com>
2020-05-30 06:29:05 -0700

    1051. Height Checker (alt solution)

--- 1051_height_checker.js
+++ 1051_height_checker.js
@@ -5,6 +5,10 @@
  * A: https://leetcode.com/problems/height-checker/discuss/603571/Javascript-and-C%2B%2B-solutions
  */
 
+// concise
+let heightChecker = A => [...A].sort((a, b) => a - b).filter((x, i) => x != A[i]).length;
+
+// verbose
 let heightChecker = (A, diff = 0) => {
     let B = [...A].sort((a, b) => a - b);
     for (let i = 0; i < A.length; ++i)
