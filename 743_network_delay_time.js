7714dec540116f47376eaddabd02648b1c8904b0
Clayton Wong <claytonjwong@gmail.com>
2020-06-04 16:15:55 -0700

    743. Network Delay Time

---
+++ 743_network_delay_time.js
@@ -0,0 +1,16 @@
+/*
+ * 743. Network Delay Time
+ *
+ * Q: https://leetcode.com/problems/network-delay-time/
+ * A: https://leetcode.com/problems/network-delay-time/discuss/656011/Javascript-and-C%2B%2B-solutions
+ */
+let networkDelayTime = (E, N, K) => {
+    let dist = Array(N + 1).fill(Infinity); // +1 for 1-based indexing
+    dist[K] = 0;
+    while (--N)
+        for (let [u, v, w] of E)
+            if (dist[v] > dist[u] + w)
+                dist[v] = dist[u] + w;
+    let max = Math.max(...dist.slice(1)); // exclude sentinel at index 0
+    return max < Infinity ? max : -1;
+};
