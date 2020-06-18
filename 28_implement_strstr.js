864e68f8ec3d01a43e133ccc904483ad16602324
Clayton Wong <claytonjwong@gmail.com>
2020-06-18 12:07:44 -0700

    28. Implement strStr()

---
+++ 28_implement_strstr.js
@@ -0,0 +1,8 @@
+/*
+ * 28. Implement strStr()
+ *
+ * Q: https://leetcode.com/problems/implement-strstr/
+ * A: https://leetcode.com/problems/implement-strstr/discuss/694155/Javascript-and-C%2B%2B-solutions
+ */
+
+let strStr = (haystack, needle) => haystack.indexOf(needle);
