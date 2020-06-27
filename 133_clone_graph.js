2389ce44d81b7f9bf40ccda42e89d6e334db08fb
Clayton Wong <claytonjwong@gmail.com>
2020-06-26 17:07:48 -0700

    133. Clone Graph (add concise solution)

--- 133_clone_graph.js
+++ 133_clone_graph.js
@@ -5,6 +5,19 @@
  * A: https://leetcode.com/problems/clone-graph/discuss/613748/Javascript-and-C%2B%2B-solutions
  */
 
+// concise
+let cloneGraph = (node, m = {}) => {
+    if (!node)
+        return null;
+    if (m[node.val])
+        return m[node.val];
+    m[node.val] = new Node(node.val);
+    for (let nei of node.neighbors)
+        m[node.val].neighbors.push(cloneGraph(nei, m));
+    return m[node.val];
+};
+
+// verbose
 let cloneGraph = (node, seen = new Map()) => {
     let go = node => {
         if (seen.has(node.val))
