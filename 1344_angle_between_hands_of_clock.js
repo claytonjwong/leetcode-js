acff4cb59fd0ba7c7ad23b1b6ed67b8c51ac9959
Clayton Wong <claytonjwong@gmail.com>
2020-07-14 08:12:15 -0700

    1344. Angle Between Hands of a Clock (verbose solution)

--- 1344_angle_between_hands_of_clock.js
+++ 1344_angle_between_hands_of_clock.js
@@ -11,7 +11,18 @@
  * @return {number}
  */
 
+// concise
 let angleClock = (h, m) => {
     let ans = Math.abs((h % 12 * 30 + m / 2.0) - (m * 6));
     return Math.min(ans, 360 - ans);
+};
+
+// verbose
+let angleClock = (h, m) => {
+    if (h == 12)
+        h = 0;
+    h = 30 * h + 0.5 * m;
+    m = 6 * m;
+    let degree = Math.max(h, m) - Math.min(h, m);
+    return Math.min(degree, 360 - degree)
 };
