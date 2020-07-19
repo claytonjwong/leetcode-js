53e993f3fc86a8d4752dbb5bea21b5e857db2445
Clayton Wong <claytonjwong@gmail.com>
2020-07-19 07:57:43 -0700

    67. Add Binary (more concise code)

--- 67_add_binary.js
+++ 67_add_binary.js
@@ -1,22 +1,20 @@
- /*
-  * 67. Add Binary
-  *
-  * Q: https://leetcode.com/problems/add-binary/
-  * A: https://leetcode.com/problems/add-binary/discuss/606766/Javascript-and-C%2B%2B-solutions
-  */
- let addBinary = (a, b, carry = 0, ans = []) => {
-    let M = a.length,
-        N = b.length;
-    let A = a.split('').reverse(),
-        B = b.split('').reverse();
-    for (let i = 0; i < Math.max(M, N); ++i) {
-        let x = Number(i < M && A[i] == '1')
-              + Number(i < N && B[i] == '1')
-              + carry;
-        ans.push(x & 1);
-        carry = Number(x > 1);
+/*
+ * 67. Add Binary
+ *
+ * Q: https://leetcode.com/problems/add-binary/
+ * A: https://leetcode.com/problems/add-binary/discuss/744220/Javascript-Python3-C%2B%2B-accumulate-sum-a%2Bb%2Bc-in-reverse
+ */
+
+let addBinary = (A, B, c = 0, ans = []) => { // ⭐️ c is the carry
+    let M = A.length,
+        N = B.length;
+    for (let i = M - 1, j = N - 1; 0 <= i || 0 <= j; --i, --j) {
+        let a = 0 <= i ? Number(A[i]) : 0,
+            b = 0 <= j ? Number(B[j]) : 0;
+        ans.push((a + b + c) % 2);
+        c = 1 < a + b + c;
     }
-    if (carry)
+    if (c)
         ans.push(1);
     return ans.reverse().join('');
 };
