8bd5c3e84078ff755e05b8ce2fdf6aac5a56dff4
Clayton Wong <claytonjwong@gmail.com>
2020-07-23 08:01:07 -0700

    1376. Time Needed to Inform All Employees (DFS + BFS)

--- 1376_time_to_inform_all_employees.js
+++ 1376_time_to_inform_all_employees.js
@@ -5,23 +5,31 @@
  * A: https://leetcode.com/problems/time-needed-to-inform-all-employees/discuss/534379/Javascript-and-C%2B%2B-solutions
  */
 
- /**
- * @param {number} n
- * @param {number} headID
- * @param {number[]} manager
- * @param {number[]} informTime
- * @return {number}
- */
-
-let numOfMinutes = (N, root, parent, cost) => {
-    let adj = [...Array(N)].map(row => []);
-    for (let i = 0; i < N; ++i)
-        if (parent[i] > -1)
-            adj[parent[i]].push(i);
-    let go = (i, max = 0) => {
-        for (let j of adj[i])
-            max = Math.max(max, cost[i] + go(j));
-        return max;
+// DFS
+let numOfMinutes = (N, root, A, cost) => {
+    let adj = [...Array(N)].map(_ => []);
+    A.forEach((u, v) => -1 < u ? adj[u].push(v) : null);
+    let go = (u, max = 0) => {
+        for (let v of adj[u])
+            max = Math.max(max, go(v));
+        return cost[u] + max;
     };
     return go(root);
+};
+
+// BFS
+let numOfMinutes = (N, root, A, cost, max = 0) => {
+    let adj = [...Array(N)].map(_ => []);
+    A.forEach((u, v) => -1 < u ? adj[u].push(v) : null);
+    let dist = Array(N).fill(0);
+    let q = [ root ];
+    while (q.length) {
+        let u = q.shift();
+        for (let v of adj[u]) {
+            dist[v] = dist[u] + cost[u];
+            max = Math.max(max, dist[v])
+            q.push(v);
+        }
+    }
+    return max;
 };
