7a550d71a92771bb82c5f626b213239f084a2695
Clayton Wong <claytonjwong@gmail.com>
2020-07-31 10:14:55 -0700

    1486. XOR Operation in an Array

--- 1486_xor_array.js
+++ 1486_xor_array.js
@@ -5,4 +5,8 @@
  * A: https://leetcode.com/problems/xor-operation-in-an-array/discuss/700800/Javascript-and-C%2B%2B-solutions
  */
 
+// recurisve
+let xorOperation = (n, x) => n == 1 ? x : x ^ xorOperation(n - 1, x + 2);
+
+// iterative
 let xorOperation = (N, start) => [...Array(N)].map((_, i) => start + 2 * i).reduce((a, b) => a ^ b , 0);
