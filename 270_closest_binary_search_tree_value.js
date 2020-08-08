e9b66bf0aec064372420d1d2c3f363c72f8f840d
Clayton Wong <claytonjwong@gmail.com>
2020-08-08 07:15:22 -0700

    270. Closest Binary Search Tree Value

--- 270_closest_binary_search_tree_value.js
+++ 270_closest_binary_search_tree_value.js
@@ -5,15 +5,15 @@
  * A2: https://leetcode.com/problems/closest-binary-search-tree-value/discuss/549446/Javascript-and-C%2B%2B-solutions
  */
 
-let closestValue = (root, target, best = Infinity, ans = -1) => {
+let closestValue = (root, T, best = Infinity) => {
     let go = root => {
-        let diff = Math.abs(target - root.val);
-        if (best > diff)
-            best = diff,
-            ans = root.val;
-        if (root.left) go(root.left);
-        if (root.right) go(root.right);
+        if (!root)
+            return;
+        if (Math.abs(root.val - T) < Math.abs(best - T))
+            best = root.val;
+        go(root.left);
+        go(root.right);
     };
     go(root);
-    return ans;
+    return best;
 };
