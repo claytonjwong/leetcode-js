50ed8bfd62ba78f69386fd256fabf7a2e9a98f4d
Clayton Wong <claytonjwong@gmail.com>
2020-08-08 13:57:00 -0700

    1540. Can Convert String in K Moves (use Object instead of Map for conciseness/readability)

--- 1540_can_convert_string_in_k_moves.js
+++ 1540_can_convert_string_in_k_moves.js
@@ -4,20 +4,20 @@
  * Q: https://leetcode.com/problems/can-convert-string-in-k-moves/
  * A: https://leetcode.com/problems/can-convert-string-in-k-moves/discuss/780454/Javascript-Python3-C%2B%2B-add-%2B-del-needs
  */
-let canConvertString = (s, t, K, need = new Map(), needs = 0) => {
+let canConvertString = (s, t, K, need = {}, needs = 0) => {
     if (s.length != t.length)
         return false;
     // ✅ add needs
     for (let i = 0; i < s.length; ++i) {
         let diff = (t[i].charCodeAt(0) - s[i].charCodeAt(0) + 26) % 26;
         if (diff)
-            need.set(diff, 1 + (need.get(diff) || 0)), ++needs;
+            need[diff] = need[diff] ? 1 + need[diff] : 1, ++needs;
     }
     // 🚫 del needs
     for (let i = 1; i <= K && needs; ++i) {
         let diff = i % 26;
-        if (need.get(diff) || 0)
-            need.set(diff, -1 + need.get(diff)), --needs;
+        if (need[diff])
+            --need[diff], --needs;
     }
     return !needs; // 🎯 no needs
 };
