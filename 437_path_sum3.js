673432fdd4c17552715d1ad1c3f7ece2c53a11b8
Clayton Wong <claytonjwong@gmail.com>
2020-08-08 07:16:13 -0700

    437. Path Sum III

--- 437_path_sum3.js
+++ 437_path_sum3.js
@@ -5,25 +5,17 @@
  * A: https://leetcode.com/problems/path-sum-iii/discuss/671026/Javascript-and-C%2B%2B-solutions
  */
 
-// concise
-let pathSum = (root, T) => {
-    let dfs = (root, sum = 0) => !root ? 0
-        : Number(sum + root.val == T) + dfs(root.left, sum + root.val) + dfs(root.right, sum + root.val);
-    let go = root => !root ? 0 : dfs(root) + go(root.left) + go(root.right);
-    return go(root);
-};
-// verbose
-let pathSum = (root, T) => {
-    let dfs = (root, sum = 0) => {
+let pathSum = (root, T, cnt = 0) => {
+    let go = (root, sum = 0) => {
         if (!root)
             return 0;
         sum += root.val;
-        return Number(sum == T) + dfs(root.left, sum) + dfs(root.right, sum);
+        return Number(sum == T) + go(root.left, sum) + go(root.right, sum);
     };
-    let go = root => {
+    let traverse = root => {
         if (!root)
             return 0;
-        return dfs(root) + go(root.left) + go(root.right);
+        return go(root) + traverse(root.left) + traverse(root.right);
     };
-    return go(root);
+    return traverse(root);
 };
