7cd11919c0b07421570c73b0f018af96dc319975
Clayton Wong <claytonjwong@gmail.com>
2020-08-10 07:03:46 -0700

    171. Excel Sheet Column Number

--- 171_excel_sheet_column_number.js
+++ 171_excel_sheet_column_number.js
@@ -2,9 +2,13 @@
  * 171. Excel Sheet Column Number
  *
  * Q: https://leetcode.com/problems/excel-sheet-column-number/
- * A: https://leetcode.com/problems/excel-sheet-column-number/discuss/594372/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/excel-sheet-column-number/discuss/594372/Javascript-Python3-C%2B%2B-1-Liners
  */
 
+// concise
+let titleToNumber = s => s.split('').reverse().map((c, i) => (c.charCodeAt(0) - 64) * Math.pow(26, i)).reduce((a, b) => a + b);
+
+// verbose
 let titleToNumber = (S, ans = 0) => {
     let N = S.length;
     for (let i = N - 1, j = 1; i >= 0; --i, j *= 26)
