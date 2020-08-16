dd5a823b6728acb1f0e01a2d2e2b79923e2b392f
Clayton Wong <claytonjwong@gmail.com>
2020-08-15 23:45:48 -0700

    884. Uncommon Words from Two Sentences

---
+++ 884_uncommon_words_from_two_sentences.js
@@ -0,0 +1,12 @@
+/*
+ * 884. Uncommon Words from Two Sentences
+ *
+ * Q: https://leetcode.com/problems/uncommon-words-from-two-sentences/
+ * A: https://leetcode.com/problems/uncommon-words-from-two-sentences/discuss/159907/Javascript-Python3-C%2B%2B-Hash-Table
+ */
+
+let uncommonFromSentences = (A, B, m = {}, ans = []) => {
+    let words = [...A.split(' '), ...B.split(' ')];
+    words.forEach(word => m[word] = 1 + (m[word] || 0));
+    return words.filter(word => m[word] == 1);
+};
