d9dd5fc1503945f9f3208fd676d1a2ad886871d3
Clayton Wong <claytonjwong@gmail.com>
2020-08-21 08:40:11 -0700

    1267. Count Servers that Communicate

--- 1267_count_servers_that_communicate.js
+++ 1267_count_servers_that_communicate.js
@@ -2,30 +2,22 @@
  * 1267. Count Servers that Communicate
  *
  * Q: https://leetcode.com/problems/count-servers-that-communicate/
- * A: https://leetcode.com/problems/count-servers-that-communicate/discuss/438394/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/count-servers-that-communicate/discuss/438394/Javascript-Python3-C%2B%2B-Count-per-Row-and-Column
  */
 
- /**
- * @param {number[][]} A
- * @return {number}
- */
-let countServers = (A, servers=[], ans=0) => {
+let countServers = (A, servers = [], cnt = 0) => {
     let M = A.length,
         N = A[0].length;
-    let row = [...Array(M)].fill(0),
-        col = [...Array(N)].fill(0);
-    for (let i=0; i < M; ++i)
-        for (let j=0; j < N; ++j)
+    let row = Array(M).fill(0),
+        col = Array(N).fill(0);
+    for (let i = 0; i < M; ++i)
+        for (let j = 0; j < N; ++j)
             if (A[i][j] == 1)
-                servers.push([i,j]),
+                servers.push([ i, j ]),
                 ++row[i],
                 ++col[j];
-    for (let [i,j] of servers)
-        if (row[i] > 1 || col[j] > 1)
-            ++ans;
-    return ans;
+    for (let [i, j] of servers)
+        if (1 < row[i] || 1 < col[j])
+            ++cnt;
+    return cnt;
 };
-
-let A = [[0,1],[1,0]];
-let ans = countServers(A);
-console.log(ans);
