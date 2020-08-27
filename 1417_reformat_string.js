5972c987480906ec31f1b076321d97eaafbc0dea
Clayton Wong <claytonjwong@gmail.com>
2020-08-27 09:37:19 -0700

    1417. Reformat The String (add solution using zip())

--- 1417_reformat_string.js
+++ 1417_reformat_string.js
@@ -1,10 +1,25 @@
- /*
-  * 1417. Reformat The String
-  *
-  * Q: https://leetcode.com/problems/reformat-the-string/
-  * A: https://leetcode.com/problems/reformat-the-string/discuss/586472/Javascript-and-C%2B%2B-solutions
-  */
- let reformat = s => {
+/*
+ * 1417. Reformat The String
+ *
+ * Q: https://leetcode.com/problems/reformat-the-string/
+ * A: https://leetcode.com/problems/reformat-the-string/discuss/586472/Javascript-Python3-C%2B%2B-zip()
+ */
+
+// concise
+let reformat = s => {
+    let digits = s.split('').filter(c => c.charCodeAt(0) < 97),   // 97 is the ordinal value of 'a'
+        letters = s.split('').filter(c => c.charCodeAt(0) >= 97);
+    if (1 < Math.abs(letters.length - digits.length))
+        return '';
+    if (digits.length < letters.length)
+        ans = _.zip(letters, digits);
+    else
+        ans = _.zip(digits, letters);
+    return ans.flat().join('');
+};
+
+// verbose
+let reformat = s => {
     let letters = s.split('').filter(c => c.charCodeAt(0) >= 97),
         digits = s.split('').filter(c => c.charCodeAt(0) < 97);
     if (Math.abs(letters.length - digits.length) > 1)
