2c06127ae397d85338a202885365ec87e30b1f5a
Clayton Wong <claytonjwong@gmail.com>
2020-09-02 18:59:55 -0700

    220. Contains Duplicate III

---
+++ 220_contains_duplicate3.js
@@ -0,0 +1,27 @@
+/*
+ * 220. Contains Duplicate III
+ *
+ * Q: https://leetcode.com/problems/contains-duplicate-iii/
+ * A: https://leetcode.com/problems/contains-duplicate-iii/discuss/825714/Javascript-Python3-C%2B%2B-Sliding-Window-with-Buckets
+ */
+
+let containsNearbyAlmostDuplicate = (A, K, T, m = {}, abs = Math.abs) => {
+    let N = A.length;
+    if (N < 2)
+        return false;
+    let bucket = x => T ? Math.floor(x / T) : Math.floor(x / (T + 1));  // ⭐️ +1 to avoid division by 0 when T == 0
+    let ok = (i, j) => m[j] != undefined && abs(m[j] - A[i]) <= T;
+    for (let i = 0; i < N; ++i) {
+		// 1. check each j-th bucket for case 1 || case 2 || case 3
+        let j = bucket(A[i]);
+        if (ok(i, j - 1) || ok(i, j) || ok(i, j + 1))  // (👈 adjacent bucket to-the-left || 🎯 same bucket || adjacent bucket to-the-right 👉)
+            return true;
+        // 2. slide window 👉
+        m[j] = A[i];                     // ✅ add current value A[i] onto the window by mapping A[i] to the j-th bucket
+        if (0 <= i - K) {
+            let end = bucket(A[i - K]);  // 🚫 remove end value A[i - K] from window by removing mapping A[i - K] to end-th bucket which "fell off the end" of window of size K
+            delete m[end];
+        }
+    }
+    return false;
+};
