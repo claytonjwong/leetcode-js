ee34c3aa40a9587cd4c762d3f7bad4c1dbe73db4
Clayton Wong <claytonjwong@gmail.com>
2020-09-05 11:46:02 -0700

    661. Image Smoother

--- 661_image_smoother.js
+++ 661_image_smoother.js
@@ -8,17 +8,11 @@
 let imageSmoother = A => {
     let M = A.length,
         N = M ? A[0].length : 0;
-    let ans = [...Array(M)].map(row => Array(N));
     let scale = (i, j, sum = A[i][j], cnt = 1) => {
-        for (let dir of [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]) {
-            let u = i + dir[0],
-                v = j + dir[1];
-            if (u < 0 || u == M || v < 0 || v == N)
-                continue;
-            sum += A[u][v], ++cnt;
-        }
+        for (let [u, v] of [[i - 1, j], [i - 1, j + 1], [i, j + 1], [i + 1, j + 1], [i + 1, j], [i + 1, j - 1], [i, j - 1], [i - 1, j - 1]])
+            if (0 <= u && u < M && 0 <= v && v < N)
+                sum += A[u][v], ++cnt;
         return Math.floor(sum / cnt);
     };
-    A.forEach((row, i) => row.forEach((col, j) => ans[i][j] = scale(i, j)));
-    return ans;
+    return A.map((row, i) => row.map((col, j) => scale(i, j)));
 };
