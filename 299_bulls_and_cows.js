0150dad2e56195ed89aebeb855b56a5ba1b85284
Clayton Wong <claytonjwong@gmail.com>
2020-09-10 07:21:48 -0700

    299. Bulls and Cows

--- 299_bulls_and_cows.js
+++ 299_bulls_and_cows.js
@@ -1,28 +1,20 @@
 /*
-
-299. Bulls and Cows
-
-Q: https://leetcode.com/problems/bulls-and-cows/
-A: https://leetcode.com/problems/bulls-and-cows/discuss/402832/Javascript-and-C%2B%2B-Solutions
-
-*/
-
-/**
- * @param {string} s
- * @param {string} t
- * @return {string}
+ * 299. Bulls and Cows
+ *
+ * Q: https://leetcode.com/problems/bulls-and-cows/
+ * A: https://leetcode.com/problems/bulls-and-cows/discuss/402832/Javascript-Python3-C%2B%2B-Map-solutions
  */
-var getHint = (s, t, cow1={}, cow2={}, bulls=0, cows=0) => {
-    for (let i=0; i < s.length; ++i)
-        if (s[i] == t[i])
-            ++bulls;
-        else
-            cow1[s[i]] = cow1[s[i]] ? cow1[s[i]] + 1 : 1,
-            cow2[t[i]] = cow2[t[i]] ? cow2[t[i]] + 1 : 1;
-    for (let c in cow1)
-        cows += Math.min(cow1[c], cow2[c]);
-    return bulls + "A" + cows + "B";
-};
 
-const ans = getHint("1122", "2211");
-console.log(ans);
+let getHint = (A, B, bull = 0, cow = 0) => {
+    let first  = Array(10).fill(0),
+        second = Array(10).fill(0);
+    for (let i = 0; i < A.length; ++i)
+        if (A[i] == B[i])
+            ++bull;               // case 1: same digit, increment bull
+        else
+            ++first[A[i]],        // case 2: diff digit, increment corresponding digit count
+			++second[B[i]];
+    for (let i = 0; i < 10; ++i)
+        cow += Math.min(first[i], second[i]);
+    return `${bull}A${cow}B`;
+};
