b812679fd586b5c66fab2dee5245052b31d23682
Clayton Wong <claytonjwong@gmail.com>
2020-09-12 12:21:39 -0700

    216. Combination Sum III

--- 216_combination_sum3.js
+++ 216_combination_sum3.js
@@ -1,5 +1,5 @@
 /*
- * Combination Sum III
+ * 216. Combination Sum III
  *
  * Q: https://leetcode.com/problems/combination-sum-iii/
  * A: https://leetcode.com/problems/combination-sum-iii/discuss/843173/Javascript-Python3-C%2B%2B-DFS-%2B-BT-solutions
@@ -8,16 +8,13 @@
 let combinationSum3 = (N, T, paths = []) => {
     let go = (n = N, t = T, path = []) => {
         if (!n && !t) {
-            paths.push([...path]);   // 🎯 unique path of N nums with T sum 👀 seen once
+            paths.push([...path]);             // 🎯 unique path of N nums with T sum
             return;
         }
         if (!n || !t)
             return;
-        for (let i = path.length ? path[path.length - 1] + 1 : 1; i < 10 && 0 <= t - i; ++i) {
-            path.push(i);            // 👀 ✅ forward-tracking
-            go(n - 1, t - i, path);  // 🚀 recursively explore path
-            path.pop();              // 👀 🚫 back-tracking
-        }
+        for (let i = path.length ? path[path.length - 1] + 1 : 1; i < 10 && 0 <= t - i; ++i)
+            go(n - 1, t - i, path.concat(i));  // 🚀 recursively explore path with implicit 👀 forward/back-tracking
         return paths;
     };
     return go();
