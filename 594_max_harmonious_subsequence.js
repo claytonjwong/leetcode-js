135f5dbef9b14133d562a7169ff1af78e552063e
Clayton Wong <claytonjwong@gmail.com>
2020-09-14 08:59:43 -0700

    594. Longest Harmonious Subsequence

--- 594_max_harmonious_subsequence.js
+++ 594_max_harmonious_subsequence.js
@@ -2,8 +2,9 @@
  * 594. Longest Harmonious Subsequence
  *
  * Q: https://leetcode.com/problems/longest-harmonious-subsequence/
- * A: https://leetcode.com/problems/longest-harmonious-subsequence/discuss/592722/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/longest-harmonious-subsequence/discuss/846353/Javascript-Python3-C%2B%2B-Map-solutions
  */
+
 let findLHS = (A, m = new Map(), max = 0) => {
     A.forEach(x => m.set(x, 1 + (m.get(x) || 0)));
     for (let [x, _] of m)
