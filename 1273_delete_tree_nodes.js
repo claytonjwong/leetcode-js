7dd8bbe1940ae5f8bafa93967c5409c3a56caecf
Clayton Wong <claytonjwong@gmail.com>
2020-09-28 13:14:06 -0700

    1273. Delete Tree Nodes (BFS prune leaves)

--- 1273_delete_tree_nodes.js
+++ 1273_delete_tree_nodes.js
@@ -2,52 +2,27 @@
  * 1273. Delete Tree Nodes
  *
  * Q: https://leetcode.com/problems/delete-tree-nodes/
- * A: https://leetcode.com/problems/delete-tree-nodes/discuss/476958/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/delete-tree-nodes/discuss/476958/Javascript-Python3-C%2B%2B-BFS-Prune-Leaves
  */
 
- // two-pass
- /*
-let deleteTreeNodes = (N, parent, value) => {
-    let children = [...Array(N)].map(row => []);
-    for (let i = 1; i < N; ++i) { // i-th child, j-th parent
-        let j = parent[i];
-        children[j].push(i);
-    }
-    let dfs = (node, sum = 0) => {
-        for (let child of children[node])
-            sum += dfs(child);
-        return value[node] += sum;
-    };
-    let bfs = (node, cnt = 1) => { // count starts at 1 to include the root node
-        for (let q = children[node]; q.length > 0; q.shift()) {
-            let cur = q[0];
-            if (value[cur] != 0)
-                q.push(...children[cur]), ++cnt;
+let deleteTreeNodes = (N, P, total, pruned = 0) => {
+    let degree = Array(N).fill(0);
+    for (let parent of P)
+        ++degree[parent];
+    let q = [];
+    degree.forEach((x, i) => !x ? q.push(i) : null);
+    let nodes = Array(N).fill(1);
+    while (q.length) {
+        let child = q.pop(),
+            parent = P[child];
+        if (!total[child]) {
+            pruned += nodes[child];
+        } else {
+            nodes[parent] += nodes[child];
+            total[parent] += total[child];
         }
-        return cnt;
-    };
-    dfs(0);
-    return bfs(0);
+        if (parent != -1 && !--degree[parent])
+            q.push(parent);
+    }
+    return N - pruned;
 };
-*/
-
-let deleteTreeNodes = (N, parent, value) => {
-    let children = [...Array(N)].map(row => []);
-    for (let i = 1; i < N; ++i)
-        children[parent[i]].push(i);
-    let dfs = (node, sum = 0) => {
-        for (let child of children[node])
-            sum += dfs(child);
-        return value[node] += sum;
-    };
-    let bfs = (node, cnt = 1) => { // count starts at 1 to include the root node
-        for (let q = children[node]; q.length > 0; q.shift())
-            if (value[q[0]] != 0)
-                q.push(...children[q[0]]), ++cnt;
-        return cnt;
-    };
-    dfs(0);
-    return bfs(0);
-};
-
-console.log(deleteTreeNodes(7, [-1,0,0,1,2,2,2], [1,-2,4,0,-2,-1,-1]));
