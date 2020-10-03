a40339ad9baa081a776364a40cc945ca37198441
Clayton Wong <claytonjwong@gmail.com>
2020-10-03 14:38:53 -0700

    1602. Find Nearest Right Node in Binary Tree (DFS)

--- 1602_nearest_right_neighbor.js
+++ 1602_nearest_right_neighbor.js
@@ -2,9 +2,26 @@
  * 1602. Find Nearest Right Node in Binary Tree
  *
  * Q: https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/
- * A: https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/discuss/875464/Javascript-Python3-C%2B%2B-BFS-solutions
+ * A: https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/discuss/875464/Javascript-Python3-C%2B%2B-DFS-and-BFS
  */
 
+// DFS
+let findNearestRightNode = (root, T, found = -1) => {
+    let go = (node = root, depth = 0) => {
+        if (!node)
+            return null;
+        if (depth == found)
+            return node;
+        if (node == T)
+            found = depth;
+        let L = go(node.left,  depth + 1),
+            R = go(node.right, depth + 1);
+        return L ? L : R ? R : null;
+    };
+    return go();
+};
+
+// BFS
 let findNearestRightNode = (root, T) => {
     let q = [root];
     while (q.length) {
