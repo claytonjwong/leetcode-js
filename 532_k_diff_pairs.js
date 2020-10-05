8003806e32e4f2f82e767179bdae26e7adb14489
Clayton Wong <claytonjwong@gmail.com>
2020-10-05 07:50:35 -0700

    532. K-diff Pairs in an Array

--- 532_k_diff_pairs.js
+++ 532_k_diff_pairs.js
@@ -2,17 +2,15 @@
  * 532. K-diff Pairs in an Array
  *
  * Q: https://leetcode.com/problems/k-diff-pairs-in-an-array/
- * A: https://leetcode.com/problems/k-diff-pairs-in-an-array/discuss/596830/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/k-diff-pairs-in-an-array/discuss/596830/Javascript-Python3-C%2B%2B-Concise-solutions
  */
 
-let findPairs = (A, K, m = new Map(), seen = new Set(), cnt = 0) => {
-    A.sort((a, b) => a - b).forEach(x => {
-        if (!K) {
-            m.set(x, 1 + (m.get(x) || 0)),
-            cnt += m.get(x) == 2;
-        } else
-            cnt += !seen.has(x) && seen.has(x - K);
+let findPairs = (A, K, seen = new Set(), pairs = new Set()) => {
+    let pair = (a, b) => pairs.add(a < b ? `${a},${b}` : `${b},${a}`);
+    for (let x of A) {
+        if (seen.has(x - K)) pair(x, x - K);
+        if (seen.has(x + K)) pair(x, x + K);
         seen.add(x);
-    });
-    return cnt;
+    }
+    return pairs.size;
 };
