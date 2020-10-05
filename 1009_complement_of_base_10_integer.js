22f121c0cb05354cd23cc1683e027439f99dee9f
Clayton Wong <claytonjwong@gmail.com>
2020-10-05 07:09:55 -0700

    1009. Complement of Base 10 Intger

--- 1009_complement_of_base_10_integer.js
+++ 1009_complement_of_base_10_integer.js
@@ -2,11 +2,13 @@
  * 1009. Complement of Base 10 Intger
  *
  * Q: https://leetcode.com/problems/complement-of-base-10-integer/
- * A: https://leetcode.com/problems/complement-of-base-10-integer/discuss/613061/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/complement-of-base-10-integer/discuss/613061/Javascript-Python3-C%2B%2B
  */
+
+let bitwiseComplement = x => parseInt(x.toString(2).split('').map(bit => bit == '1' ? '0' : '1').join(''), 2);
 
 let bitwiseComplement = (x, i = 1) => {
     while (i < x)
-        i *= 2;
+        i <<= 1;
     return !x ? !x : ~x & (i - 1);
 };
