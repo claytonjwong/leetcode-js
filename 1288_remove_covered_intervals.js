27852ddb530f109d6ed032aae8a9f1b35a6ff495
Clayton Wong <claytonjwong@gmail.com>
2020-10-05 07:09:10 -0700

    1288. Remove Covered Intervals

--- 1288_remove_covered_intervals.js
+++ 1288_remove_covered_intervals.js
@@ -2,28 +2,20 @@
  * 1288. Remove Covered Intervals
  *
  * Q: https://leetcode.com/problems/remove-covered-intervals/
- * A: https://leetcode.com/problems/remove-covered-intervals/discuss/457523/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/remove-covered-intervals/discuss/457523/Javascript-Python3-C%2B%2B-Sort-%2B-Linear-Scan
  */
 
-/*
-let removeCoveredIntervals = (A, max = 0, cnt = 0) => {
-    A.sort((a, b) => a[0] - b[0]);
-    for (let i = 0; i < A.length; ++i)
-        if (A[i][1] > max)
-            max = A[i][1], ++cnt;
-    return cnt;
-}
-*/
-
-let removeCoveredIntervals = (A, max = 0, cnt = 0) => {
-    A.sort((a, b) => a[0] - b[0]);
-    return A.filter(x => {
-        if (x[1] <= max)
-            return false;
-        max = x[1];
-        return true;
-    }).length;
-}
-
-let ans = removeCoveredIntervals([[1,4], [3,6], [2,8]]);
-console.log(ans);
+let removeCoveredIntervals = (A, x = 0) => {
+    A.sort(([ a, b ], [ c, d ]) => a == c ? d - b : a - c);
+    let ok = (a, b, c, d) => a <= c && d <= b;
+    let N = A.length,
+        i = 0,
+        j = 1;
+    while (j < N)
+        if (ok(...A[i], ...A[j]))
+            ++x,
+            ++j;
+        else
+            i = j++;
+    return N - x;
+};
