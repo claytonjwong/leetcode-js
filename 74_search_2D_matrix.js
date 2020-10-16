fa475953c34e0ba562341ea6c2b27cd336b1cc4e
Clayton Wong <claytonjwong@gmail.com>
2020-10-16 10:41:46 -0700

    74. Search a 2D Matrix

--- 74_search_2D_matrix.js
+++ 74_search_2D_matrix.js
@@ -2,27 +2,27 @@
  * 74. Search a 2D Matrix
  *
  * Q: https://leetcode.com/problems/search-a-2d-matrix/
- * A: https://leetcode.com/problems/search-a-2d-matrix/discuss/605294/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/search-a-2d-matrix/discuss/605294/Kt-Js-Py3-Cpp-Upper-Bound-(ie.-Binary-Search)
  */
 
-let searchMatrix = (A, T) => {
-    let M = A.length,
-        N = M ? A[0].length : 0;
-    if (!M || !N)
-        return false;
-    let find = (A, i, j) => {
-        while (i < j) {
-            let k = Math.floor((i + j) / 2);
-            if (T > A[k])
-                i = k + 1;
-            else
-                j = k;
-        }
-        return i;
-    };
-    let rows = A.map(row => row[N - 1]);
-    let i = find(rows, 0, M - 1);
-    let j = find(A[i], 0, N - 1);
-    return A[i][j] == T;
+let upperBound = (A, T) => {
+    let N = A.length,
+        i = 0,
+        j = N;
+    while (i < j) {
+        let k = Math.floor((i + j + 1) / 2);
+        if (A[k] <= T)
+            i = k;
+        else
+            j = k - 1;
+    }
+    return j + 1;
 };
-console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,50]], 21));
+let searchMatrix = (A, T) => {
+    if (!A.length || !A[0].length)
+        return false;
+    let first = A.map(row => row[0]);
+    let row = upperBound(first, T) - 1;
+    let col = upperBound(A[row], T) - 1;
+    return A[row][col] == T;
+};
