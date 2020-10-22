556d15dcd0ca373f1d062c48ac0e7bb6475ea8f0
Clayton Wong <claytonjwong@gmail.com>
2020-10-22 10:34:10 -0700

    111. Minimum Depth of Binary Tree

--- 111_min_depth_binary_tree.js
+++ 111_min_depth_binary_tree.js
@@ -2,10 +2,11 @@
  * 111. Minimum Depth of Binary Tree
  *
  * Q: https://leetcode.com/problems/minimum-depth-of-binary-tree/
- * A: https://leetcode.com/problems/minimum-depth-of-binary-tree/discuss/553010/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/minimum-depth-of-binary-tree/discuss/553010/Kt-Js-Py3-Cpp-DFS-%2B-BFS
  */
 
-var minDepth = root => {
+// DFS
+let minDepth = root => {
     let go = (root, depth = 1) => {
         if (!root.left && !root.right)
             return depth;
@@ -14,4 +15,23 @@ var minDepth = root => {
         return Math.min(L, R);
     };
     return root ? go(root) : 0;
+};
+
+// BFS
+let minDepth = (root, depth = 1) => {
+    if (!root)
+        return 0;
+    let q = [ root ];
+    while (q.length) {
+        let k = q.length;
+        while (k--) {
+            let cur = q.shift();
+            if (!cur.left && !cur.right)
+                return depth;
+            if (cur.left)  q.push(cur.left);
+            if (cur.right) q.push(cur.right);
+        }
+        ++depth;
+    }
+    return -1;
 };
