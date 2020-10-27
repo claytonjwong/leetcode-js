48ce9a94aa28c3a2a0ce4da27366497e9ea09994
Clayton Wong <claytonjwong@gmail.com>
2020-10-27 07:09:59 -0700

    142. Linked List Cycle II

--- 142_linked_list_2.js
+++ 142_linked_list_2.js
@@ -2,10 +2,10 @@
  * 142. Linked List Cycle II
  *
  * Q: https://leetcode.com/problems/linked-list-cycle-ii/
- * A: https://leetcode.com/problems/linked-list-cycle-ii/discuss/523559/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/linked-list-cycle-ii/discuss/523559/Kt-Js-Py3-Cpp-Seen-ListNodes
  */
 
-let detectCycle = (head, i = 0, seen = new Set()) => {
+let detectCycle = (head, seen = new Set()) => {
     while (head) {
         if (seen.has(head))
             return head;
