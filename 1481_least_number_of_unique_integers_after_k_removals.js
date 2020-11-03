a10952c0adc7dd4c62bc1ad76b9ccb7ee5647154
Clayton Wong <claytonjwong@gmail.com>
2020-11-03 08:16:05 -0700

    1481. Least Number of Unique Integers after K Removals

--- 1481_least_number_of_unique_integers_after_k_removals.js
+++ 1481_least_number_of_unique_integers_after_k_removals.js
@@ -2,10 +2,11 @@
  * 1481. Least Number of Unique Integers after K Removals
  *
  * Q: https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/
- * A: https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/discuss/686410/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/discuss/686410/Kt-Js-Py3-Cpp-Greedy-Drop-K-Least-Frequent-via-Map
  */
-let findLeastNumOfUniqueInts = (A, K, m = new Map(), all = []) => {
+
+let findLeastNumOfUniqueInts = (A, K, m = new Map()) => {
     A.forEach(x => m.set(x, 1 + (m.get(x) || 0)));
-    [...m.entries()].sort((a, b) => a[1] - b[1]).forEach(([x, n]) => all.push(...Array(n).fill(x))); // sort all by ascending frequency (x occurs n times)
-    return new Set(all.slice(K)).size; // greedily drop first K least freq nums and return size of remaining set
+    A.sort((a, b) => m.get(a) == m.get(b) ? a - b : m.get(a) - m.get(b));
+    return new Set(A.slice(K)).size
 };
