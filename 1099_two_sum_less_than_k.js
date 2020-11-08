b8a9ceb8c3834c8a147ca426703960a35e384794
Clayton Wong <claytonjwong@gmail.com>
2020-11-08 08:40:05 -0700

    1099. Two Sum Less Than K

--- 1099_two_sum_less_than_k.js
+++ 1099_two_sum_less_than_k.js
@@ -2,14 +2,32 @@
  * 1099. Two Sum Less Than K
  *
  * Q: https://leetcode.com/problems/two-sum-less-than-k/
- * A: https://leetcode.com/problems/two-sum-less-than-k/discuss/603797/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/two-sum-less-than-k/discuss/603797/Kt-Js-Py3-Cpp-Greedy-%2B-Brute-Force-solutions
  */
 
-let twoSumLessThanK = (A, K, max = -1) => {
+// greedy
+let twoSumLessThanK = (A, K, best = -1) => {
+    A.sort((a, b) => a - b);
+    let N = A.length,
+        i = 0,
+        j = N - 1;
+    while (i < j) {
+        if (A[i] + A[j] < K) {
+            best = Math.max(best, A[i] + A[j]);
+            ++i;
+        } else {
+            --j;
+        }
+    }
+    return best;
+};
+
+// brute-force
+let twoSumLessThanK = (A, K, best = -1) => {
     let N = A.length;
     for (let i = 0; i < N; ++i)
         for (let j = i + 1; j < N; ++j)
-            if (max < A[i] + A[j] && A[i] + A[j] < K)
-                max = A[i] + A[j];
-    return max;
+            if (A[i] + A[j] < K)
+                best = Math.max(best, A[i] + A[j]);
+    return best;
 };
