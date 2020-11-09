da40f64e3e7f63fc71d8975b4e26294ebd6310d5
Clayton Wong <claytonjwong@gmail.com>
2020-11-09 13:39:40 -0700

    1504. Count Submatrices With All Ones

--- 1504_cnt_submatrices_all_ones.js
+++ 1504_cnt_submatrices_all_ones.js
@@ -2,11 +2,13 @@
  * 1504. Count Submatrices With All Ones
  *
  * Q: https://leetcode.com/problems/count-submatrices-with-all-ones/
- * A: https://leetcode.com/problems/count-submatrices-with-all-ones/discuss/762330/Javascript-Python3-C%2B%2B-brute-force
+ * A: https://leetcode.com/problems/count-submatrices-with-all-ones/discuss/762330/Kt-Js-Py3-Cpp-Brute-Force
  */
 
-let numSubmat = (A, ans = 0) => {
-    let go = (row, col, M, N, cnt = 0) => {
+let numSubmat = (A, total = 0) => {
+    let go = (row, col, cnt = 0) => {
+        let M = A.length,
+            N = A[0].length;
         for (let i = row; i < M; ++i)
             for (let j = col; j < N; ++j)
                 if (A[i][j])
@@ -19,6 +21,6 @@ let numSubmat = (A, ans = 0) => {
         N = A[0].length;
     for (let i = 0; i < M; ++i)
         for (let j = 0; j < N; ++j)
-            ans += go(i, j, M, N); // 🎯 count of all submatrices starting with i,j as top-left corner
-    return ans;
+            total += go(i, j); // 🎯 count of all submatrices starting with i,j as top-left corner
+    return total;
 };
