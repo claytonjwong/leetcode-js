113a88606f7bbe2efc608ab7722d15a9129d38a7
Clayton Wong <claytonjwong@gmail.com>
2020-11-10 07:26:14 -0700

    832. Flipping an Image

--- 832_flipping_an_image.js
+++ 832_flipping_an_image.js
@@ -2,7 +2,7 @@
  * 832. Flipping an Image
  *
  * Q: https://leetcode.com/problems/flipping-an-image/
- * A: https://leetcode.com/problems/flipping-an-image/discuss/131721/4-liner-C%2B%2B-reverse()-and-transform()
+ * A: https://leetcode.com/problems/flipping-an-image/discuss/131721/Kt-Js-Py3-Cpp-1-Liners
  */
 
 let flipAndInvertImage = A => A.map(row => row.reverse().map(x => x ^ 1));
