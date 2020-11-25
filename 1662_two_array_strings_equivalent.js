49486e9aca97e853066f470699cb56de9a2a8bbf
Clayton Wong <claytonjwong@gmail.com>
2020-11-25 13:15:40 -0700

    1662. Check If Two String Arrays are Equivalent

---
+++ 1662_two_array_strings_equivalent.js
@@ -0,0 +1,8 @@
+/*
+ * 1662. Check If Two String Arrays are Equivalent
+ *
+ * Q: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+ * A: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/discuss/948963/Kt-Js-Py3-Cpp-1-Liners
+ */
+
+let arrayStringsAreEqual = (A, B) => A.join('') == B.join('');
