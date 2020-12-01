3d5d8faa57a28ac5627c6d2cbb696389af9d1071
Clayton Wong <claytonjwong@gmail.com>
2020-12-01 06:32:24 -0700

    243. Shortest Word Distance

--- 243_shortest_word_distance.js
+++ 243_shortest_word_distance.js
@@ -1,19 +1,16 @@
 /*
  * 243. Shortest Word Distance
  *
- * Q: https://leetcode.com/problems/shortest-word-distance-iii/
- * A: https://leetcode.com/problems/shortest-word-distance/discuss/670999/Javascript-and-C%2B%2B-solutions
+ * Q: https://leetcode.com/problems/shortest-word-distance/
+ * A: https://leetcode.com/problems/shortest-word-distance/discuss/670999/Kt-Js-Py3-Cpp-Last-Seen-K-th-Index
  */
 
-let shortestDistance = (A, s, t, min = Infinity) => {
-    let N = A.length,
-        i = -1,
-        j = -1;
-    for (let k = 0; k < N; ++k) {
-        if (A[k] == s) i = k;
-        if (A[k] == t) j = k;
-        if (0 <= i && 0 <= j)
-            min = Math.min(min, Math.abs(i - j));
-    }
-    return min;
+let shortestDistance = (A, s, t, i = -1, j = -1, best = Infinity) => {
+    A.forEach((word, k) => {
+        if (word == s) i = k;
+        if (word == t) j = k;
+        if (-1 < i && -1 < j)
+            best = Math.min(best, Math.abs(i - j));
+    });
+    return best;
 };
