406e185e493c832e3ebc4d626c4cd2bfae72630a
Clayton Wong <claytonjwong@gmail.com>
2020-12-18 14:46:21 -0700

    334. Increasing Triplet Subsequence (concise)

--- 334_increasing_triplet_subsequence.js
+++ 334_increasing_triplet_subsequence.js
@@ -2,16 +2,13 @@
  * 334. Increasing Triplet Subsequence
  *
  * Q: https://leetcode.com/problems/increasing-triplet-subsequence/
- * A: https://leetcode.com/problems/increasing-triplet-subsequence/discuss/977048/Kt-Js-Py3-Cpp-Track-Two-Minimums
+ * A: https://leetcode.com/problems/increasing-triplet-subsequence/discuss/977048/Kt-Js-Py3-Cpp-Construct-a-b-c
  */
 
-let increasingTriplet = (A, a = Infinity, b = Infinity) => {
+let increasingTriplet = (A, a = Infinity, b = Infinity, c = Infinity) => {
     for (let x of A)
-        if (x <= a)
-            a = x;
-        else if (x <= b)
-            b = x;
-        else
-            return true;
-    return false;
+        if      (x <= a) a = x;
+        else if (x <= b) b = x;
+        else if (x <= c) c = x;
+    return c < Infinity;
 };
