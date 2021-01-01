92de74289d3084e2eda70704ecc0c6318689208f
Clayton Wong <claytonjwong@gmail.com>
2021-01-01 09:08:11 -0700

    1640. Check Array Formation Through Concatenation

--- 1640_create_array.js
+++ 1640_create_array.js
@@ -5,6 +5,7 @@
  * A: https://leetcode.com/problems/check-array-formation-through-concatenation/discuss/919391/Kt-Js-Py3-Cpp-Do-we-%22have%22-what-we-%22need%22
  */
 
+// queue
 let canFormArray = (need, have, q = []) => {
     for (let x of need) {
         if (q.length) {
@@ -25,4 +26,19 @@ let canFormArray = (need, have, q = []) => {
             return false;
     }
     return true;
+};
+
+// make
+let canFormArray = (need, have, m = new Map(), make = []) => {
+    have.forEach((A, i) => m.set(A[0], i));
+    let i = 0,
+        N = need.length;
+    while (i < N) {
+        if (!m.has(need[i]))
+            return false;
+        let j = m.get(need[i]);
+        make.push(...have[j]);
+        i += have[j].length;
+    }
+    return _.zip(need, make).every(pair => pair[0] == pair[1]);
 };
