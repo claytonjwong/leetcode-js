2ac1d1ec7f00b504207c08d67d02397c20b31d34
Clayton Wong <claytonjwong@gmail.com>
2021-01-04 09:44:37 -0700

    1711. Count Good Meals (more concise)

--- 1711_count_good_meals.js
+++ 1711_count_good_meals.js
@@ -5,12 +5,12 @@
  * A: https://leetcode.com/problems/count-good-meals/discuss/1001864/Kt-Js-Py3-Cpp-Map-%2B-Brute-Force-Search
  */
 
-let countPairs = (A, m = new Map(), mod = 1e9 + 7, cnt = 0) => {
-    A.map(BigInt).forEach(x => {
-        for (let t = BigInt(1); t <= 100 * 1e9; t *= 2n) {
+let countPairs = (A, m = new Map(), cnt = 0) => {
+    A.forEach(x => {
+        for (let t = 1; t <= (1 << 21); t <<= 1) {
             let y = t - x;
             if (m.has(y))
-                cnt = (cnt + m.get(y)) % mod;
+                cnt = (cnt + m.get(y)) % (1e9 + 7);
         }
         m.set(x, 1 + (m.get(x) || 0));
     });
