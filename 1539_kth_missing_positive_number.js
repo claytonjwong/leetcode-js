6a7c12d9fefb07fb03d1ca84e47f9c63fbce5be0
Clayton Wong <claytonjwong@gmail.com>
2021-01-06 08:28:51 -0700

    1539. Kth Missing Positive Number

--- 1539_kth_missing_positive_number.js
+++ 1539_kth_missing_positive_number.js
@@ -2,12 +2,26 @@
  * 1539. Kth Missing Positive Number
  *
  * Q: https://leetcode.com/problems/kth-missing-positive-number/
- * A: https://leetcode.com/problems/kth-missing-positive-number/discuss/780016/Javascript-Python3-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/kth-missing-positive-number/discuss/780016/Kt-Js-Py3-Cpp-Seen-Set-%2B-Concise
  */
 
+// seen
 let findKthPositive = (A, K) => {
     let seen = new Set(A);
     for (let i = 1;; ++i)
         if (!seen.has(i) && !--K)
             return i;
+};
+
+// concise
+let findKthPositive = (A, k) => {
+    let i = 0,
+        x = 0;
+    while (k && i < A.length) {
+        if (++x == A[i])
+            ++i;
+        else
+            --k;
+    }
+    return x + k;
 };
