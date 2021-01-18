9a2b558f27d60faf2a36c4254d10f4a5eac5171b
Clayton Wong <claytonjwong@gmail.com>
2021-01-18 08:34:14 -0700

    1679. Max Number of K-Sum Pairs (more concise)

--- 1679_ksum_pairs.js
+++ 1679_ksum_pairs.js
@@ -5,17 +5,13 @@
  * A: https://leetcode.com/problems/max-number-of-k-sum-pairs/discuss/962118/Kt-Js-Py3-Cpp-Map
  */
 
-let maxOperations = (A, k, m = new Map(), pairs = 0) => {
+let maxOperations = (A, T, m = new Map(), cnt = 0) => {
     for (let x of A) {
-        let y = k - x;
-        if (!m.has(y)) {
-            m.set(x, 1 + (m.get(x) || 0));
-            continue;
-        }
-        ++pairs;
-        m.set(y, m.get(y) - 1);
-        if (!m.get(y))
-            m.delete(y);
+        let y = T - x;
+        if (m.has(y) && m.get(y))
+            m.set(y, -1 + m.get(y)), ++cnt;
+        else
+            m.set(x,  1 + (m.get(x) || 0));
     }
-    return pairs;
+    return cnt;
 };
