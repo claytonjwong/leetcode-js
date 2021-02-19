49d566a2520471b8ed25ad6a19af78c24634523a
Clayton Wong <claytonjwong@gmail.com>
2021-02-19 10:01:16 -0700

    338. Counting Bits

--- 338_counting_bits.js
+++ 338_counting_bits.js
@@ -2,26 +2,12 @@
  * 338. Counting Bits
  *
  * Q: https://leetcode.com/problems/counting-bits/
- * A: https://leetcode.com/problems/counting-bits/discuss/657068/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/counting-bits/discuss/657068/Kt-Js-Py3-Cpp-Dynamic-Programming
  */
 
-// verbose
-let countBits = (num, pow = 0, ans = [0]) => {
-    for (let i = 1; i <= num; ++i)
-        if (!(i & (i - 1)))
-            ans.push(1), pow = i; // max pow2 so far
-        else
-            ans.push(1 + ans[i - pow]); // +1 for max pow2 + the prev ans w/o max pow2
+let countBits = N => {
+    let ans = [ 0 ];
+    for (let i = 1; i <= N; ++i)
+        ans.push(ans[i >> 1] + (i & 1));
     return ans;
 };
-
-// concise
-let countBits = (num, pow = 0, ans = [0]) => {
-    for (let i = 1; i <= num; ++i) {
-        if (!(i & (i - 1))) pow = i; // max pow2 so far
-        ans.push(1 + ans[i - pow]); // +1 for max pow2 + the prev ans w/o max pow2
-    }
-    return ans;
-};
-
-console.log(countBits(5));
